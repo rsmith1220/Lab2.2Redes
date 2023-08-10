469059b17bc89210c834baa2558402397c255b6a
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2023-08-10 13:07:45 -0600

    changes and additions

--- Correccion/encoder.java
+++ Correccion/encoder.java
@@ -21,6 +21,63 @@ public class encoder {
         
         return xorResult;
     }
+    
+    public static int[] encodeData(int[] data) {
+        int m = calculateParityBits(data.length);
+        int[] encodedData = new int[data.length + m];
 
+        // inicializar los parity bits con -1
+        for (int i = 0; i < m; i++) {
+            encodedData[(int) Math.pow(2, i) - 1] = -1;
+        }
+
+        int dataIndex = 0;
+        for (int i = 0; i < encodedData.length; i++) {
+            if (isPowerOfTwo(i + 1)) {
+                continue; 
+            }
+            encodedData[i] = data[dataIndex];
+            dataIndex++;
+        }
+
+        // llenar bits de paridad
+        for (int i = 0; i < m; i++) {
+            int parityBitIndex = (int) Math.pow(2, i) - 1;
+            int parity = calculateParity(encodedData, i);
+            encodedData[parityBitIndex] = parity;
+        }
+        
+        return encodedData;
+    }
+    
+    // Check if a number is a power of two
+    public static boolean isPowerOfTwo(int number) {
+        return (number & (number - 1)) == 0;
+    }
+    
+    public static String Devolucion(int [] info){
+        
+        int[] encodedData = encodeData(info);
+
+        // Simulate transmission by introducing an error
+        encodedData[2] = 0;
+
+        // Decode received data and correct errors
+        int[] decodedData = new int[info.length];
+        int m = calculateParityBits(decodedData.length);
+
+        for (int i = 0; i < decodedData.length; i++) {
+            if (!isPowerOfTwo(i + 1)) {
+                decodedData[i] = encodedData[i];
+            }
+        }
+
+        // Output the results
+        System.out.println("Input Data: " + Arrays.toString(info));
+        System.out.println("Encoded Data: " + Arrays.toString(encodedData));
+        System.out.println("Decoded Data: " + Arrays.toString(decodedData));
+        
+        return "________";
+    }
     
 }
